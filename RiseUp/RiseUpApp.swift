46e6d4605dc654832e23df751aed66af4fffa96d
Vanessa Mercado <vanessa.mercado24@gmail.com>
2023-09-03 18:01:55 +0800

    Initial Commit

---
+++ RiseUp/RiseUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseUpApp.swift
+//  RiseUp
+//
+//  Created by Vanessa Mercado on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RiseUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
