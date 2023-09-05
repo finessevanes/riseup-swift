325c76e943f285244cdf19085b4da9700d7e505f
Vanessa Mercado <vanessa.mercado24@gmail.com>
2023-09-05 11:39:08 +0800

    center time picker

--- RiseUp/TimePickerView.swift
+++ RiseUp/TimePickerView.swift
@@ -7,6 +7,9 @@ class TimePickerView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        timePicker.translatesAutoresizingMaskIntoConstraints = false
+
+
 
         // Configure date picker
         timePicker.locale = Locale(identifier: "en_GB")
@@ -16,6 +19,14 @@ class TimePickerView: UIViewController {
 
         // Add date picker to the view
         self.view.addSubview(timePicker)
+        
+        // this activates where it will sit
+        // since i only want it to be center leaving it at the top
+        NSLayoutConstraint.activate([
+            timePicker.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+//            timePicker.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+
 
         // Register for date picker changes
         timePicker.addTarget(self, action: #selector(timeChanged(_:)), for: .valueChanged)
