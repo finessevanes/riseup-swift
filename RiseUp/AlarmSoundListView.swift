5ab1d10a653c02358f82887498ec140f12fe7b82
Vanessa Mercado <vanessa.mercado24@gmail.com>
2023-09-06 17:41:26 +0800

    add list of sounds saved with play/stop functionality icon

---
+++ RiseUp/AlarmSoundListView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct AlarmSoundListView: View {
+    let sounds: [String: String] = ["Turja": "alarm", "David Goggins": "whos-gonna-carry-the-boats"] // Replace with your actual mappings
+    @State private var currentlyPlaying: String? = nil
+    
+    var body: some View {
+        List(sounds.keys.sorted(), id: \.self) { displayName in
+            HStack {
+                Text(displayName)
+                Spacer()
+                Button(action: {
+                    if let soundName = sounds[displayName] {
+                        if currentlyPlaying == soundName {
+                            SoundUtility.stopSound()
+                            currentlyPlaying = nil
+                        } else {
+                            SoundUtility.playSound(soundName: soundName)
+                            currentlyPlaying = soundName
+                        }
+                    }
+                }) {
+                    Image(systemName: (currentlyPlaying == sounds[displayName]) ? "stop.fill" : "play.fill")
+                }
+            }
+        }
+    }
+    
+}
+
+
+struct AlarmSoundListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmSoundListView()
+    }
+}
